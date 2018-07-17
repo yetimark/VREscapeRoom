d549ba0fbc0ae73e0c57b88f91f64a80317a4e0d
YetiMark <blaine.andrada@cuw.edu>
2018-07-17 12:10:45 -0500

    Initial commit

---
+++ Assets/Scripts/DoorMover.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorMover : MonoBehaviour
+{
+
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.name == "Key1")
+        {
+            Debug.Log("The key has hit the door!");
+            this.gameObject.transform.Rotate(0, 90, 0);
+        }
+    }
+
+}
